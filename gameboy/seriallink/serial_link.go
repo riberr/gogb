deeadb318babf70f5b0055a05610f8a8daf38aae
Gustaf Johansson <gustaf.johansson@cpacsystems.se>
2023-11-09 16:39:42 +0100

    add seriallink package

---
+++ gameboy/seriallink/serial_link.go
@@ -0,0 +1,10 @@
+package seriallink
+
+// https://gbdev.io/pandocs/Serial_Data_Transfer_(Link_Cable).html
+// https://gbdev.gg8.se/wiki/articles/Serial_Data_Transfer_(Link_Cable)
+type SerialLink struct {
+}
+
+func (sl SerialLink) Tick() {
+
+}
